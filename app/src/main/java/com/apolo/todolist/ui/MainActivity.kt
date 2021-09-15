d97f6c62b3fddcaac9fec3379796747b6cc64b10
Jean-Carlos-A-Souza <jean.carlos.alb.souza@gmail.com>
2021-09-15 18:43:47 -0300

    Code Update

---
+++ app/src/main/java/com/apolo/todolist/ui/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.apolo.todolist.ui
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.apolo.todolist.databinding.ActivityMainBinding
+import com.apolo.todolist.datasource.TaskDataSource
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTask.adapter = adapter
+
+        updateList()
+
+        insertListeners()
+    }
+
+    private fun insertListeners() {
+       binding.fab.setOnClickListener{
+           startActivityForResult(Intent(this, AddTaskActivity::class.java), CREATE_NEW_TASK)
+       }
+        adapter.listenerEdit = {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            startActivityForResult(intent, CREATE_NEW_TASK)
+        }
+        adapter.listenerDelete = {
+            TaskDataSource.deleteTask(it)
+            updateList()
+
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == CREATE_NEW_TASK && resultCode == Activity.RESULT_OK) updateList()
+
+        }
+
+    private fun updateList(){
+       val list = TaskDataSource.getList()
+
+        binding.emptyEnclude.emptyState.visibility = if(list.isEmpty()){
+            View.VISIBLE
+        }else{
+            View.GONE
+        }
+
+        adapter.submitList(list)
+    }
+
+    companion object {
+        private const val CREATE_NEW_TASK = 1000
+    }
+}
